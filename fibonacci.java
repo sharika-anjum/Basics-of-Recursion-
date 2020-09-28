6e43b45be547ab58a86ca9cccaaf3bde8857b13c
Sharika Anjum Mondal <54097782+sharika-anjum@users.noreply.github.com>
2020-09-28 20:01:54 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class fibonacci {
+    public static void main(String[] args)
+    {
+        int pairs = fib(12);
+        System.out.println(pairs);
+
+    }
+    public static int fib(int n){
+
+        if(n==0 || n==1){
+            return 1;
+        }else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
+}
+//very high time complexity
+//to reduce this time complexity, we increase its space complexity and optimize the code
