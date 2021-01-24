d5932eb3547df5e9c8d5dbc20db053bf58240734
Sharika Anjum Mondal <54097782+sharika-anjum@users.noreply.github.com>
2021-01-24 13:39:19 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class area {
+
+    public static void main(String args[]){
+        double radius = 2.02;
+        double ar = (22/7) * Math.pow(radius,2);
+        System.out.println("The area of the circle is " + ar);
+    }
+}
