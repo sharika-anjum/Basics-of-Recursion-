3f051a816a06cb015c8739866f5791d75f1a768d
Sharika Anjum Mondal <54097782+sharika-anjum@users.noreply.github.com>
2021-02-02 20:00:21 +0530

    Add files via upload

--- Gross.java
+++ Gross.java
@@ -3,24 +3,31 @@ import java.util.*;
 public class Gross {
     public static void main(String args[])
     {
-        float basic_salary,da,hra,da1,hra1,GrossPayment;
-
         Scanner scan=new Scanner(System.in);
 
+        System.out.println("Enter Employee name: ");
+        String name=scan.nextLine();
+
+        System.out.println("Enter Employee ID: ");
+        int id=scan.nextInt();
+
         System.out.println("Enter Basic Salary Of Employee: ");
-        basic_salary=scan.nextFloat();
+        float basic_salary=scan.nextFloat();
 
-        System.out.println("Enter Basic DA Of Employee: ");
-        da1=scan.nextFloat();
+        System.out.println("Enter Medical Allowance Of Employee: ");
+        float ma =scan.nextFloat();
 
-        System.out.println("Enter Basic HRA Of Employee: ");
-        hra1=scan.nextFloat();
 
-        da = (da1 * basic_salary) / 100;
-        hra = (hra1 * basic_salary) / 100;
-
-        GrossPayment = basic_salary + da + hra;
-
+        float da = (50 * basic_salary) / 100;
+        float hra = (25 * basic_salary) / 100;
+        float pf = (10*basic_salary)/100;
+        float pt = 100;
+        float GrossPayment = basic_salary + da + hra + ma;
+        float deduction = pf+pt;
+        float net = GrossPayment - deduction;
+        System.out.println("Employee name: "+name);
+        System.out.println("Employee ID: "+id);
         System.out.println("Gross Salary Of Employee: "+GrossPayment);
+        System.out.println("Net Salary Of Employee: "+net);
     }
 }
