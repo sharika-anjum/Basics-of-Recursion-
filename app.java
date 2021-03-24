ea8651793000103c74b476e51437057c78fbc374
Sharika Anjum Mondal <54097782+sharika-anjum@users.noreply.github.com>
2021-03-24 17:14:25 +0530

    Add files via upload

---
+++ app.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.applet.Applet;
+import java.awt.Graphics;
+public class app extends Applet{
+
+    public void paint(Graphics g){
+        g.drawString("welcome",150,150);
+    }
+
+}
